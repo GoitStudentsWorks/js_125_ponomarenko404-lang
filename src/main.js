74cae68c98fd181dd12b25356bb90c8b96235dfb
Alona Volvach <alena.volvach.aleksandrovna@gmail.com>
2026-04-06 12:02:36 +0200

    feedback section update svg

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import './css/modern-normalize.css';
+import './js/feedback.js';
 import './css/styles.css';
-import './css/feedback.css';
 import './js/order-modal.js';
 import './js/furniture-modal.js';
 import './js/header.js';
 import './js/furniture-list.js';
 import './js/faq.js';
-import './js/feedback.js';
+
